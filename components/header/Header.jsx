efe0644accc0f88af3be7e09f3d7a2096d9b3a5e
John Muthua <caveomuthama@gmail.com>
2021-10-12 16:33:17 +0300

    first commit

---
+++ components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      {/* sidebar */}
+      {/* main side */}
+      {/* far left side */}
+    </div>
+  );
+}
+
+export default Header;
